704687950ddab556f6fb57aae2a483de744563d7
Brad Gibson <b2b@humanenginuity.com>
2019-01-17 05:02:16 +0000

    tweaked clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
 #![warn(clippy::all)]
 #![forbid(unsafe_code)] // Do not remove!  Explicitly change to #![allow(unsafe_code)] to use `unsafe` keyword.
-#![allow(clippy::match_bool,)]
-#![deny(warnings)]
 #![forbid(overflowing_literals)]
+#![deny(warnings)]
+// Uncomment before ship to reconcile use of possibly redundant crates and uncover possible debug remnants
+//#![warn(clippy::multiple_crate_versions, clippy::print_stdout, clippy::unimplemented, clippy::use_debug, missing_docs)]
 // vvv Safety-critical application lints (pedantic: use for safety-critical applications only) vvv
 #![deny(clippy::cast_possible_truncation, clippy::cast_possible_wrap, clippy::cast_precision_loss,
-clippy::cast_sign_loss, clippy::float_cmp_const, clippy::indexing_slicing, clippy::integer_arithmetic,
-clippy::maybe_infinite_iter, clippy::option_unwrap_used, clippy::result_unwrap_used)]
+        clippy::cast_sign_loss, clippy::float_cmp_const, clippy::indexing_slicing, clippy::integer_arithmetic,
+        clippy::maybe_infinite_iter, clippy::option_unwrap_used, clippy::result_unwrap_used)]
 // ^^^ End of safety-critical lint section ^^^
-// Uncomment before ship to reconcile use of possibly redundant crates and uncover possible debug remnants
-// #![warn(clippy::multiple_crate_versions, clippy::print_on_stdout, clippy::unimplemented, clippy::use_debug)]
+#![allow(clippy::match_bool,)]
+
 mod error;
 #[cfg(test)]
 mod unit_tests;
