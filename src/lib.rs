a08be0b76fb4d311e9850baa62541b2a7d0c252f
Brad Gibson <b2b@humanenginuity.com>
2019-03-20 08:43:31 -0700

    tweaked default warnings; added rustfmt configuration

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
-#![warn(clippy::all)]
-#![forbid(unsafe_code)] // Do not remove!  Explicitly change to #![allow(unsafe_code)] to use `unsafe` keyword.
+#![warn(clippy::all, clippy::nursery, clippy::pedantic, clippy::cargo)]
 #![forbid(overflowing_literals)]
+// To use `unsafe` keyword, change to `#![allow(unsafe_code)]` (do not remove); aids auditing.
+#![forbid(unsafe_code)]
+#![allow(clippy::match_bool)]
+// Safety-critical application lints
+#![deny(
+    clippy::pedantic,
+    clippy::float_cmp_const,
+    clippy::indexing_slicing,
+    clippy::integer_arithmetic,
+    clippy::option_unwrap_used,
+    clippy::result_unwrap_used
+)]
+// Uncomment before ship to reconcile use of possibly redundant crates, debug remnants, missing license file, and more
+//#![warn(clippy::cargo, clippy::restriction, missing_docs, warnings)]
 //#![deny(warnings)]
-//#![deny(missing_docs)]
-// Uncomment before ship to reconcile use of possibly redundant crates and uncover possible debug remnants
-//#![warn(clippy::multiple_crate_versions, clippy::print_stdout, clippy::unimplemented, clippy::use_debug)]
-// vvv Safety-critical application lints (pedantic: use for safety-critical applications only) vvv
-#![deny(clippy::cast_possible_truncation, clippy::cast_possible_wrap, clippy::cast_precision_loss,
-clippy::cast_sign_loss, clippy::float_cmp_const, clippy::indexing_slicing, clippy::integer_arithmetic,
-clippy::maybe_infinite_iter, clippy::option_unwrap_used, clippy::result_unwrap_used)]
-// ^^^ End of safety-critical lint section ^^^
-#![allow(clippy::match_bool,)]
 
 mod consts;
 mod error;
