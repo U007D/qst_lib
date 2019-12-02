eaaadb2d4b6d77b1afe354c0e6a8c1fd4f5116bb
Brad Gibson <brad@tpfs.io>
2019-12-02 15:30:14 -0800

    convert crate to thiserror

--- src/consts/msg/en_us.rs
+++ src/consts/msg/en_us.rs
@@ -1,2 +1,3 @@
 #![allow(dead_code)]
-pub const ERR_SAMPLE: &str = "This is a sample error message";
+pub const ERR_INVALID_INT_REPR: &str = "Error: String contents are not a valid representation of \
+                                        an integer";
