ec8b81548626769514257a9d2d1307cfd3c4b5fc
u007d <b2b@humanenginuity.com>
2020-04-30 06:09:16 -0700

    updated lints, rustfmt.toml; replaced newtype `Error` definition with conventional `struct` definition

--- src/consts/msg/en_us.rs
+++
@@ -1,3 +0,0 @@
-#![allow(dead_code)]
-pub const ERR_INVALID_INT_REPR: &str = "Error: String contents are not a valid representation of \
-                                        an integer";
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-#![warn(clippy::all, clippy::nursery, clippy::pedantic, rust_2018_idioms)]
-#![forbid(bare_trait_objects)]
-#![allow(clippy::match_bool)]
 // To use the `unsafe` keyword, change to `#![allow(unsafe_code)]` (do not remove); aids auditing.
 #![forbid(unsafe_code)]
+#![forbid(bare_trait_objects)]
+#![warn(clippy::all, clippy::nursery, clippy::pedantic, rust_2018_idioms)]
 // Safety-critical application lints
 #![deny(
     clippy::pedantic,
@@ -12,8 +11,13 @@
     clippy::option_unwrap_used,
     clippy::result_unwrap_used
 )]
-
-// Uncomment before ship to reconcile use of possibly redundant crates, debug remnants, missing license files and more
+#![allow(
+    clippy::match_bool,
+    clippy::iter_nth_zero,
+    clippy::module_name_repetitions
+)]
+// Uncomment before ship to reconcile use of possibly redundant crates, debug remnants, missing
+// license files and more
 //#![warn(clippy::cargo, clippy::restriction, missing_docs, warnings)]
 //#![deny(warnings)]
 
