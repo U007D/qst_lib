8ed060fdcd7df80d83fe3eba2becbf8c059ffb13
Brad Gibson <brad@tpfs.io>
2020-10-22 03:56:02 -0700

    fixed `forbid` lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,27 @@
-// To use the `unsafe` keyword, change to `#![allow(unsafe_code)]` (do not remove); aids auditing.
-#![forbid(unsafe_code)]
-#![forbid(bare_trait_objects)]
 #![warn(clippy::all, clippy::nursery, clippy::pedantic, rust_2018_idioms)]
 // Safety-critical application lints
 #![deny(
+    clippy::pedantic,
     clippy::float_cmp_const,
     clippy::indexing_slicing,
     clippy::integer_arithmetic,
     clippy::unwrap_used
 )]
 #![allow(
+    clippy::implicit_return,
     clippy::iter_nth_zero,
     clippy::match_bool,
     clippy::missing_errors_doc,
     clippy::module_name_repetitions
 )]
+// To use the `unsafe` keyword, change to `#![allow(unsafe_code)]` (do not remove); aids auditing.
+#![forbid(unsafe_code)]
+#![forbid(bare_trait_objects)]
 // Uncomment before ship to reconcile use of possibly redundant crates, debug remnants, missing
 // license files and more
-//#![warn(clippy::cargo, clippy::restriction, missing_docs, warnings)]
-//#![deny(clippy::missing_errors_doc, warnings)]
+// #![allow(clippy::blanket_clippy_restriction_lints)]
+// #![warn(clippy::cargo, clippy::restriction, missing_docs, warnings)]
+// #![allow(clippy::implicit_return)]
 
 mod consts;
 mod error;
